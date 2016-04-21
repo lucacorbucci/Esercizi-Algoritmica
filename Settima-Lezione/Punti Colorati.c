0742e319ed3ab99e4fc3611b90c8480707a46cdc
Luca <tankado95@users.noreply.github.com>
2016-04-22 00:58:22 +0200

    Ora funziona

--- Settima-Lezione/Punti Colorati.c
+++ Settima-Lezione/Punti Colorati.c
@@ -14,17 +14,34 @@ typedef struct _rettangolo{
   int z;
 } rettangolo;
 
+
+//Compare per le struct
+int comparestruct (void const *a, void const *b){
+  punto *a1=*(punto **)a;
+  punto *b1=*(punto **)b;
+  return ((a1->c)-(b1->c));
+}
+
+
+/*
+Funzione che confronta ogni punto e vede se questo appartiene al rettangolo o no
+*/
 void check(punto **point, rettangolo **rett, int n, int m){
-  int i=0, conta=0,k=0;
+  int i=0, conta=0,k=0, temp=-1;
   for(i=0;i<m;i++){
     conta=0;
+    temp=-1;
+    
     for(k=0;k<n;k++){
       if(point[k]->x >= rett[i]->x
         && point[k]->x <= rett[i]->t
         && point[k]->y >= rett[i]->y
-        && point[k]->y <= rett[i]->z)
+        && point[k]->y <= rett[i]->z
+        && temp!=point[k]->c){
 
+        temp=point[k]->c;
         conta++;
+      }
     }
     printf("%d\n",conta);
   }
@@ -38,6 +55,9 @@ int main(){
   punto ** point;
   rettangolo ** rett;
 
+  /*
+  Prendo n e m e poi alloco il punto e il rettangolo necessario.
+  */
   scanf("%d",&n);
   scanf("%d",&m);
   point=(punto **)malloc(sizeof(punto *)*n);
@@ -45,31 +65,29 @@ int main(){
 
   for(i=0;i<n;i++){
     point[i]=(punto *)malloc(sizeof(punto));
-    scanf("%d %d %d",&a, &b, &c);
-    if(a>0 && b>0){
-    point[i]->x=a;
-    point[i]->y=b;
-    point[i]->c=c;
+    scanf("%d %d %d",&point[i]->x, &point[i]->y, &point[i]->c);
   }
-}
 
   for(i=0;i<m;i++){
     rett[i]=(rettangolo *)malloc(sizeof(rettangolo));
     scanf("%d %d %d %d", &rett[i]->x,&rett[i]->y,&rett[i]->t, &rett[i]->z);
   }
 
+  //Ordinamento della struct con i punti
+  qsort(point,n,sizeof(punto *),comparestruct);
+
+
   check(point, rett, n, m);
 
-
-  //Stampa di prova di tutti gli elementi presenti nelle due strutture.
-
+  //Free sulla memoria
   for(i=0;i<n;i++){
-    printf("%d %d %d\n",point[i]->x, point[i]->y, point[i]->c);
+    free(point[i]);
   }
   for(i=0;i<m;i++){
-    printf("%d %d %d %d\n", rett[i]->x,rett[i]->y,rett[i]->t, rett[i]->z);
+    free(rett[i]);
   }
-
+  free(point);
+  free(rett);
 
 
 return 0;
